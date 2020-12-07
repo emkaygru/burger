83f93c352310ff68d1a22d57b23ce57f751bae0a
Emily Groenendal <emilylauder@gmail.com>
2020-12-07 18:55:15 -0500

    updating orm functions

--- config/orm.js
+++ config/orm.js
@@ -6,9 +6,35 @@ var connection = require("../config/connection.js");
 
 // function updateOne() { }
 
+function printQuestionMarks(num) {
+    var array = [];
 
+    for (var i = 0; i < num; i++) {
+        array.push("?");
+    }
+
+    return array.toString();
+};
+
+function objToSql() {
+    var array = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            array.push(key + "=" + value);
+        }
+
+    }
+    return array.toString();
+}
 var orm = {
-    all: function (tableInput, cb) {
+    selectAll: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function (err, result) {
             if (err) {
@@ -17,7 +43,7 @@ var orm = {
             cb(result);
         });
     },
-    create: function (table, cols, vals, cb) {
+    insertOne: function (table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
 
         queryString += " (";
@@ -38,7 +64,7 @@ var orm = {
         });
     },
     // An example of objColVals would be
-    update: function (table, objColVals, condition, cb) {
+    updateOne: function (table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
 
         queryString += " SET ";
