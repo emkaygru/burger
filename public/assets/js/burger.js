904ee455defad7e056ee5a2495aa1934d4c4dd6c
Emily Groenendal <emilylauder@gmail.com>
2020-12-07 19:53:09 -0500

    created burger.js - first commit

---
+++ public/assets/js/burger.js
@@ -0,0 +1,18 @@
+$(function () {
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+
+        var newBurger = {
+            burger_name = $("#new-burger").val().trim(),
+            devoured: 0
+        };
+
+
+    })
+
+
+
+
+
+})
