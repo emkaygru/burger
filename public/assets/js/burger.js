4e2fee4de5c8a0d889e71808ab9af9545c5cdaad
Emily Groenendal <emilylauder@gmail.com>
2020-12-07 20:01:52 -0500

    updating burger.js function API calls

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,15 +1,49 @@
 $(function () {
-    $(".create-form").on("submit", function (event) {
-        event.preventDefault();
+    $('.eatburger').on('click', function (e) {
+        e.preventDefault();
+
+        let id = $(this).data('id');
+        let devouredState = {
+            devoured: 1
+        };
+
+        $.ajax('/api/burgers/' + id, {
+            type: PUT,
+            data: devouredState
+        }).then(function () {
+            console.log('Burger has been DEVOURED, BITCHEZ!', devouredState);
+            location.reload();
+        })
+    });
+
+    $(''.create - form ').on('
+        submit ', function (e) {
+        e.preventDefault();
 
 
-        var newBurger = {
-            burger_name = $("#new-burger").val().trim(),
+        let newBurger = {
+            burger_name = $('#new-burger').val().trim(),
             devoured: 0
         };
 
+        $.ajax('/api/burgers', {
+            type: 'POST',
+            data: newBurger
+        }).then(function () {
+            console.log('Added a New Burger!');
+            location.reload();
+        });
 
-    })
+    });
+
+$('.trashburger').on('click', function (e) {
+
+
+
+
+})
+
+
 
 
 
