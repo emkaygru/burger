9593d7284220694c063fade9e266dbb247f7fc76
Emily Groenendal <emilylauder@gmail.com>
2020-12-07 20:08:06 -0500

    burger.js API calls

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    // eat burger click function - on click, set the devoured state to 1 
     $('.eatburger').on('click', function (e) {
         e.preventDefault();
 
@@ -6,18 +8,20 @@ $(function () {
         let devouredState = {
             devoured: 1
         };
-
+        // PUT request 
         $.ajax('/api/burgers/' + id, {
             type: PUT,
             data: devouredState
         }).then(function () {
             console.log('Burger has been DEVOURED, BITCHEZ!', devouredState);
+            // reload the list once you have updated it 
             location.reload();
         })
     });
 
-    $(''.create - form ').on('
-        submit ', function (e) {
+    // create form -- on click submit function 
+    $('.createform ').on('submit', function (e) {
+        // preventDefault on a submit event 
         e.preventDefault();
 
 
@@ -25,28 +29,31 @@ $(function () {
             burger_name = $('#new-burger').val().trim(),
             devoured: 0
         };
-
+        // POST request 
         $.ajax('/api/burgers', {
             type: 'POST',
             data: newBurger
         }).then(function () {
             console.log('Added a New Burger!');
+            // reload the list once you have updated it 
             location.reload();
         });
 
     });
+    // trash the burger once you have deleted it
+    $('.trashburger').on('click', function (e) {
+        let id = $(this).data('id');
 
-$('.trashburger').on('click', function (e) {
+        // DELETE request 
+        $.ajax('/api/burgers/' + id, {
+            type: DELETE,
+        }).then(function () {
+            console.log('Burger has been YEETED INTO SPACE', id);
+            // reload the list once you have updated it 
+            location.reload();
+        })
 
-
-
-
-})
-
-
-
-
-
+    })
 
 
 })
