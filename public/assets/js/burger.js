7d7bbbf2a48e2e3c22d16a4053863dda8a865b62
Emily Groenendal <emilylauder@gmail.com>
2020-12-14 21:27:17 -0500

    updating js and index function

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,59 +1,54 @@
 $(function () {
+  // create form -- on click submit function
+  $("#addBurger").on("submit", function (e) {
+    // preventDefault on a submit event
+    e.preventDefault();
 
-    // eat burger click function - on click, set the devoured state to 1 
-    $('.eatburger').on('click', function (e) {
-        e.preventDefault();
-
-        let id = $(this).data('id');
-        let devouredState = {
-            devoured: 1
-        };
-        // PUT request 
-        $.ajax('/api/burgers/' + id, {
-            type: PUT,
-            data: devouredState
-        }).then(function () {
-            console.log('Burger has been DEVOURED, BITCHEZ!', devouredState);
-            // reload the list once you have updated it 
-            location.reload();
-        })
+    let newBurger = {
+      burger_name: $("#burgerName").val().trim(),
+      devoured: 0,
+    };
+    // POST request
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("Added a New Burger!");
+      // reload the list once you have updated it
+      location.reload();
     });
+  });
 
-    // create form -- on click submit function 
-    $('.createform ').on('submit', function (e) {
-        // preventDefault on a submit event 
-        e.preventDefault();
+  // eat burger click function - on click, set the devoured state to 1
+  $(".eatburger").on("click", function (e) {
+    e.preventDefault();
 
-
-        let newBurger = {
-            burger_name = $('#new-burger').val().trim(),
-            devoured: 0
-        };
-        // POST request 
-        $.ajax('/api/burgers', {
-            type: 'POST',
-            data: newBurger
-        }).then(function () {
-            console.log('Added a New Burger!');
-            // reload the list once you have updated it 
-            location.reload();
-        });
-
+    let id = $(this).data("id");
+    let devouredState = {
+      devoured: 1,
+    };
+    // PUT request
+    $.ajax("/api/burgers/" + id, {
+      type: PUT,
+      data: devouredState,
+    }).then(function () {
+      console.log("Burger has been DEVOURED, BITCHEZ!", devouredState);
+      // reload the list once you have updated it
+      location.reload();
     });
-    // trash the burger once you have deleted it
-    $('.trashburger').on('click', function (e) {
-        let id = $(this).data('id');
+  });
 
-        // DELETE request 
-        $.ajax('/api/burgers/' + id, {
-            type: DELETE,
-        }).then(function () {
-            console.log('Burger has been YEETED INTO SPACE', id);
-            // reload the list once you have updated it 
-            location.reload();
-        })
+  // trash the burger once you have deleted it
+  $(".trashburger").on("click", function (e) {
+    let id = $(this).data("id");
 
-    })
-
-
-})
+    // DELETE request
+    $.ajax("/api/burgers/" + id, {
+      type: DELETE,
+    }).then(function () {
+      console.log("Burger has been YEETED INTO SPACE", id);
+      // reload the list once you have updated it
+      location.reload();
+    });
+  });
+});
