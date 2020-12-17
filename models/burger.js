7aa1eb8f99ffe3ada0cfe989fc52dae0edd5a724
Emily Groenendal <emilylauder@gmail.com>
2020-12-16 21:50:12 -0500

    updating routes on server and burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,27 @@
-const orm = require('../config/orm.js');
+const orm = require("../config/orm.js");
 
 var burger = {
-    selectAll: function (cb) {
-        orm.selectAll("burgers", function (res) {
-            cb(res);
-        });
-    },
-    // The variables cols and vals are arrays.
-    insertOne: function (cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function (res) {
-            cb(res);
-        });
-    },
-    updateOne: function (objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    },
-    deleteOne: function (condition, cb) {
-        orm.deleteOne("burgers", condition, function (res) {
-            cb(res);
-        });
-    }
+  selectAll: (cb) => {
+    orm.selectAll("burgers", (res) => {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: (cols, vals, cb) => {
+    orm.insertOne("burgers", cols, vals, (res) => {
+      cb(res);
+    });
+  },
+  updateOne: (objColVals, condition, cb) => {
+    orm.updateOne("burgers", objColVals, condition, (res) => {
+      cb(res);
+    });
+  },
+  deleteOne: (condition, cb) => {
+    orm.deleteOne("burgers", condition, (res) => {
+      cb(res);
+    });
+  },
 };
 
 module.exports = burger;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const sql = require('mysql');
+const express = require("express");
+const sql = require("mysql");
 
-
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -10,25 +9,30 @@ var app = express();
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+);
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
