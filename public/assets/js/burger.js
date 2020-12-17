e72c6e79ea7fed6125372541c0b8caf43e5cb082
Emily Groenendal <emilylauder@gmail.com>
2020-12-16 19:54:15 -0500

    updating burger.js functions

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,58 +1,3 @@
-// $(function () {
-//   // create form -- on click submit function
-//   $("#addBurger").on("submit", function (e) {
-//     // preventDefault on a submit event
-//     e.preventDefault();
-
-//     let newBurger = {
-//       burger_name: $("#burgerName").val().trim(),
-//       devoured: 0,
-//     };
-//     // POST request
-//     $.ajax("/api/burgers", {
-//       type: "POST",
-//       data: newBurger,
-//     }).then(function () {
-//       console.log("Added a New Burger!");
-//       // reload the list once you have updated it
-//       location.reload();
-//     });
-//   });
-
-//   // eat burger click function - on click, set the devoured state to 1
-//   $(".eatburger").on("click", function (e) {
-//     e.preventDefault();
-
-//     let id = $(this).data("id");
-//     let devouredState = {
-//       devoured: 1,
-//     };
-//     // PUT request
-//     $.ajax("/api/burgers/" + id, {
-//       type: PUT,
-//       data: devouredState,
-//     }).then(function () {
-//       console.log("Burger has been DEVOURED, BITCHEZ!", devouredState);
-//       // reload the list once you have updated it
-//       location.reload();
-//     });
-//   });
-
-//   // trash the burger once you have deleted it
-//   $(".trashburger").on("click", function (e) {
-//     let id = $(this).data("id");
-
-//     // DELETE request
-//     $.ajax("/api/burgers/" + id, {
-//       type: DELETE,
-//     }).then(function () {
-//       console.log("Burger has been YEETED INTO SPACE", id);
-//       // reload the list once you have updated it
-//       location.reload();
-//     });
-//   });
-// });
-
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
   $(".devoured").on("click", function (e) {
@@ -86,7 +31,7 @@ $(function () {
       type: "POST",
       data: newBurger,
     }).then(function () {
-      console.log("created new burger");
+      console.log("created new burger", newBurger);
       // Reload the page to get the updated list
       location.reload();
     });
