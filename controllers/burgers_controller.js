d8d3a4bd4f14a84aeb934e3e50e89299d250b8d6
Emily Groenendal <emilylauder@gmail.com>
2020-12-17 15:49:41 -0500

    updating burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,11 +17,9 @@ router.get("/", (req, res) => {
 });
 
 router.post("/api/burgers", (req, res) => {
-  burger.insertOne(["burger_name"], [req.body.burger_name], (result) => {
+  burger.insertOne(["burger_name"], [req.body.name], (result) => {
     // Send back the ID of the new quote
-    res.json({
-      id: result.insertId,
-    });
+    res.redirect("/");
   });
 });
 
