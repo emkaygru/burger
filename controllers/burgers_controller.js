66fea6969988c31e94ca0f2bfb19756475c70849
Em <emilylauder@gmail.com>
2021-01-31 17:42:10 -0500

    updated burger controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,30 +15,45 @@ router.get("/", (req, res) => {
   });
 });
 
-//POST
-//insertOne function
-router.post("/", function (req, res) {
-  burger.insertOne(["burger_name"], [req.body.name], function () {
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne(["burger_name"], [req.body.name], (result) => {
     res.redirect("/");
   });
 });
 
-//PUT
-//updateOne function
-router.put("/:id", function (req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.updateOne(
     {
-      devoured: req.body.devoured,
+      devoured: true,
     },
     condition,
-    function () {
-      res.redirect("/");
+    () => {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
     }
   );
+});
+
+router.delete("/api/burgers/:id", (req, res) => {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+
+  burger.deleteOne(condition, () => {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
 
 // Export routes for server.js to use.
